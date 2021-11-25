e99241ade94cdb4966fe9c7431f6db6c0e3e7696
Anderson Silva <anderson_silva11000@outlook.com>
2021-11-24 22:50:00 -0300

    Alteração prevenção e add imagens

--- prevencao.php
+++ prevencao.php
@@ -1,22 +1,122 @@
 <?php include_once('includes/header.php'); ?>
 <!-- inicio do conteudo -->
 
-<main class="row">
-    <article class="col-6 offset-1">
-      <h2>Parte do Porpeta</h2>
-      <p>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui."</p>
-      <p>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui."</p>
-      <p>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui."</p>
+<main class="row row-cols-1 row-cols-md-2 g-4">
 
-    </article>
+<div class="col">
+    <div class="card">
+      <img src="imagens/1" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">USO DA MÁSCARA</h5>
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#teste0" aria-expanded="true" aria-controls="collapseOne">
+                  Veja aqui
+                </button>
+        
+        <section class="accordion-body accordion-collapse collapse" aria-labelledby="headingOne" id="teste0" >
+          <p>O uso da máscara é fundamental para combater o Covid-19.</p>
+          <p> 1.	A máscara é individual e não deve ser compartilhada. </p>
+          <p> 2.	Use sempre que precisar sair de casa e lembre de levar uma reserva.</p>
+          <p> 3.	Deve cobrir o nariz e a boca e ser usada de forma permanente no rosto, ou seja, não deve ser manipulada para o queixo ou pendurada em uma das orelhas, por exemplo.</p>
+          <p> 4.	O ideal é que cada pessoa tenha pelo menos duas opções de máscaras de pano, em caso rompimento do elástico ou de umidade excessiva na mesma. </p>
+          <p> 5.	Tenha cuidado com o descarte das máscaras descartáveis. Vede a máscara com uma sacola e jogue diretamente na lixeira. </p>
+        </section>
+      </div>
+    </div>
+</div>
 
-    <aside class="col-3 offset-1">
-      <h3>TEXTO EXEMPLO</h3>
-      <p>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui."</p>
-      <p>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui."</p>
+<div class="col">
+    <div class="card">
+      <img src="imagens/2" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">HIGIENENIZAÇÃO</h5>
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#teste1" aria-expanded="true" aria-controls="collapseOne">
+                  Veja aqui
+                </button>
+        
+        <section class="accordion-body accordion-collapse collapse" aria-labelledby="headingOne" id="teste1" >
+          <p>Medidas básicas na rotina diária, como lavar as mãos são fundamentais para a redução do contágio.</p>
+          <p> O vírus também pode ser transferido de uma superfície para outra por meio das mãos contaminadas, o que facilita a transmissão por contato indireto. Consequentemente, a higienização das mãos é extremamente importante para evitar a disseminação do vírus causador da covid-19. Ela também interrompe a transmissão de outros vírus e bactérias que causam resfriado comum, gripe e pneumonia, reduzindo assim o impacto geral da doença. No caso específico da higienização, é recomendado o uso do álcool a 70% como desinfetante e antissépticos para as mãos. </p>
+        </section>
+      </div>
+    </div>
+</div>
 
-    </aside>
+<div class="col">
+    <div class="card">
+      <img src="imagens/3" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">CUIDADOS NO AMBIENTE DE TRABALHO</h5>
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#teste2" aria-expanded="true" aria-controls="collapseOne">
+                  Veja aqui
+                </button>
+        
+        <section class="accordion-body accordion-collapse collapse" aria-labelledby="headingOne" id="teste2" >
+          <p>No ambiente de trabalho é importante estar atento à higiene. O recomendado é que as empresas e organizações disponibilizem recursos para a higienização das mãos nos locais de trabalho, incluindo água, sabonete líquido, toalha de papel descartável e lixeira, com abertura que não demande contato manual, ou sanitizante adequado para as mãos, como álcool a 70%.</p>
+          <p>Quanto ao distanciamento, a recomendação é manter o espaço mínimo de um metro entre os trabalhadores e o público. Os funcionários devem usar a máscara cirúrgica ou de tecido e adotar divisórias impermeáveis ou fornecer proteção facial do tipo viseira plástica ou óculos de proteção.</p>
+          <p>Nas refeições, a orientação é para que os trabalhadores sejam distribuídos em horários diferentes para o uso do refeitório e seja mantido o espaço mínimo de um metro entre as pessoas nas filas e mesas. No caso dos banheiros, deve-se evitar a aglomeração.</p>
+        </section>
+      </div>
+    </div>
+</div>
+
+<div class="col">
+    <div class="card">
+      <img src="imagens/4" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">CUIDADOS COMPLEMENTARES</h5>
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#teste3" aria-expanded="true" aria-controls="collapseOne">
+                  Veja aqui
+                </button>
+        
+        <section class="accordion-body accordion-collapse collapse" aria-labelledby="headingOne" id="teste3" >
+          <p>O álcool 70% é um importante aliado de higienização na luta contra a Covid-19. Mas é importante estar atento ao seu uso para evitar queimaduras. </p>
+          <p>Ao usar a máscara, passe protetor solar para evitar marcas de queimaduras de sol indesejáveis no rosto.</p>
+        </section>
+      </div>
+    </div>
+</div>
+
+
+<div class="col">
+    <div class="card">
+      <img src="imagens/5" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">DISTANCIAMENTO SOCIAL</h5>
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#teste4" aria-expanded="true" aria-controls="collapseOne">
+                  Veja aqui
+                </button>
+        
+        <section class="accordion-body accordion-collapse collapse" aria-labelledby="headingOne" id="teste4" >
+          <p>Limitar o contato próximo entre pessoas infectadas e outras pessoas é importante para reduzir as chances de transmissão. Seguir as medidas de distanciamento diminui a velocidade de transmissão do vírus. </p>
+          <p>Trata-se de uma estratégia importante quando há indivíduos já infectados, mas ainda assintomáticos ou oligossintomáticos, que não se sabem portadores da doença e não estão em isolamento.</p>
+          <p>Além disso, recomenda-se a manutenção de uma distância física mínima de pelo menos 1 metro de outras pessoas, especialmente daquelas com sintomas respiratórios e um grande número de pessoas (aglomerações) tanto ao ar livre quanto em ambientes fechados.</p>
+          <p>Garantir uma boa ventilação em ambientes internos também é uma medida importante para prevenir a transmissão em ambientes coletivos.</p>
+        </section>
+      </div>
+    </div>
+</div>
+
+<div class="col">
+    <div class="card">
+      <img src="imagens/1" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">IMAGEM MERAMENTE ILUSTRATIVA</h5>
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#teste5" aria-expanded="true" aria-controls="collapseOne">
+                  Veja aqui
+                </button>
+        
+        <section class="accordion-body accordion-collapse collapse" aria-labelledby="headingOne" id="teste5" >
+          <p>Limitar o contato próximo entre pessoas infectadas e outras pessoas é importante para reduzir as chances de transmissão. Seguir as medidas de distanciamento diminui a velocidade de transmissão do vírus. </p>
+          <p>Trata-se de uma estratégia importante quando há indivíduos já infectados, mas ainda assintomáticos ou oligossintomáticos, que não se sabem portadores da doença e não estão em isolamento.</p>
+          <p>Além disso, recomenda-se a manutenção de uma distância física mínima de pelo menos 1 metro de outras pessoas, especialmente daquelas com sintomas respiratórios e um grande número de pessoas (aglomerações) tanto ao ar livre quanto em ambientes fechados.</p>
+          <p>Garantir uma boa ventilação em ambientes internos também é uma medida importante para prevenir a transmissão em ambientes coletivos.</p>
+        </section>
+      </div>
+    </div>
+</div>
+
 </main>
+
 
 
 <!-- fim do conteudo -->
