f0a7014f549ad8c515a3a3cf55b415feb25d1d56
Rafael Roslan <rafaelroslan@gmail.com>
2021-11-28 22:10:47 -0300

    parte rafael

---
+++ dissem_brasil.php
@@ -0,0 +1,184 @@
+<?php include_once('includes/header.php'); ?>
+<!-- inicio do conteudo -->
+<main class="container text-justify">
+
+
+    <?php include_once('includes/dissem_nav.php'); ?>
+
+    <div class="row mt-4">
+        <article class="col-10 col-sm-12 col-md-12 col-lg-6 offset-1">
+            <h2 class="text-center">Disseminação do covid no Brasil</h2>
+            <p>O Brasil é o único país que, com população superior a 100 milhões, possui um sistema de saúde universal,
+                integral e gratuito. Apesar dos cortes no orçamento da saúde, esperava-se que o sistema de saúde do
+                Brasil colocasse o país em uma boa posição para mitigar a pandemia COVID-19. No entanto, o Brasil é um
+                dos países mais atingidos pela COVID-19. Em 11 de março de 2021, 11.277.717 casos e 272.889 mortes foram
+                relatados. Esses representam 9,5% e 10,4% dos casos e óbitos mundiais, respectivamente; no entanto, o
+                Brasil compartilha apenas 2,7% da população mundial. No final de maio de 2020, a América Latina foi
+                declarada o epicentro da pandemia COVID-19, principalmente por causa do Brasil.</p>
+            <p>Sem uma estratégia nacional coordenada, as respostas locais variaram em forma, intensidade, duração e
+                horários de início e fim. Após múltiplas introduções do SARS-CoV-2, o Brasil teve uma fase epidêmica
+                inicial &#40;15 de fevereiro a 18 de março de 2020&#41; com circulação restrita, precedida por
+                circulação viral não detectada. Embora a propagação inicial tenha sido determinada pelas desigualdades
+                socioeconômicas existentes, a falta de uma resposta coordenada, eficaz e equitativa provavelmente
+                alimentou a propagação espacial generalizada do SARS-CoV-2.</p>
+            <p>Em todos os estados, demorou menos de um mês entre o primeiro caso e a primeira morte; apenas 11 dias no
+                Amazonas e 21 em São Paulo. À medida que estados e municípios impuseram e amenizaram medidas restritivas
+                em diferentes momentos, a mobilidade populacional facilitou a circulação do vírus e atuou como
+                desencadeador da disseminação da doença. Em tal cenário, respostas imediatas e equitativas, coordenadas
+                em nível federal, são imperativas para evitar a propagação rápida do vírus. No entanto, a resposta do
+                COVID-19 no Brasil não foi imediata nem justa.</p>
+            <p>Em 11 de março de 2021, o Brasil já registrava 40% do total de mortes de COVID-19 ocorridas em 2020. Em
+                janeiro de 2021, Manaus testemunhou um pico de casos e hospitalizações, um colapso do sistema
+                hospitalar, incluindo falta de oxigênio para pacientes. O número de mortos é insuportável, pois Manaus
+                já registrou 39,8% mais mortes por COVID-19 em 2021 do que em 2020.</p>
+            <p>fonte: <a
+                    href="https://www.ufrgs.br/microbiologando/2021/04/19/publicado-artigo-sobre-o-padrao-de-disseminacao-da-covid-19-no-brasil/">ufrgs/microbiologando</a>
+            </p>
+        </article>
+
+        <aside class="col-4 offset-1">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col" colspan="3">Local</th>
+                        <th scope="col">Casos</th>
+                        <th scope="col">Mortes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td colspan="3">São Paulo</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td colspan="3">Minas Gerais</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td colspan="3">Paraná</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td colspan="3">Rio grande do Sul</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td colspan="3">Rio de Janeiro</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">6</th>
+                        <td colspan="3">Bahia</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">7</th>
+                        <td colspan="3">Santa Catarina</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">8</th>
+                        <td colspan="3">Ceará</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">9</th>
+                        <td colspan="3">Goiás</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">10</th>
+                        <td colspan="3">Pernanbuco</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">11</th>
+                        <td colspan="3">Espírito Santo</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">12</th>
+                        <td colspan="3">Pará</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">13</th>
+                        <td colspan="3">Mato Grosso</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">14</th>
+                        <td colspan="3">Distrito federal</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">15</th>
+                        <td colspan="3">Paraíba</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">16</th>
+                        <td colspan="3">Amazonas</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">17</th>
+                        <td colspan="3">Rio Grande do Norte</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">18</th>
+                        <td colspan="3">Mato Grosso do Sul</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">19</th>
+                        <td colspan="3">Maranhão</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">20</th>
+                        <td colspan="3">Piauí</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">21</th>
+                        <td colspan="3">Sergipe</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+        </aside>
+    </div>
+</main>
+
+<!-- fim do conteudo -->
+<?php include_once('includes/footer.php'); ?>
