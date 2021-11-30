d79c62a66259a6b8a97ea597951349298a1c89b9
Felipe <goesfelipesantos@gmail.com>
2021-11-30 13:58:21 -0300

    alterações css Variantes

--- var_delta.php
+++ var_delta.php
@@ -1,16 +1,18 @@
 <?php include_once('includes/header.php'); ?>
+<link rel="stylesheet" href="css/variante.css">
 <main>
 <?php include_once('includes/var_nav.php'); ?>
 <div class="var_alinha">
-    <h3>Variante Delta</h3>
+    <h2>Variante Delta</h2>
     <img class="justify-content-center" src="imagens/corona roxo.png">
 
+    <div class="texto">
         <p> <b>Quem é:</b> Detectada em outubro de 2020 na Índia, foi rotulada como variante de preocupação recentemente, em maio.</p>
         <p> <b> Mutações:</b> Mais de uma dúzia, mas duas estão no centro das atenções. A E484Q, alteração semelhante à notada nas variantes Beta e Gama, que poderia ajudar o vírus a escapar dos anticorpos; e a L452R, também ligada à resposta imune.</p>
         <p> <b>Comportamento:</b> Parece ser a mais contagiosa até agora. Estima-se que ela seja entre 40 e 60% mais transmissível do que a Alfa, tanto que acabou provocando surtos onde esta já era predominante, como o Reino Unido, o que motivou alertas do governo britânico. Um possível risco maior de hospitalizações está em investigação, mas ainda não foi confirmado.</p>
         <p> <b>Resposta às vacinas:</b> Estudos mostram redução importante na ação de anticorpos neutralizantes com apenas uma dose das vacinas de Pfizer e AstraZeneca. Com duas doses, porém, a proteção se mantém. Outra pesquisa, do governo do Reino Unido, mostra que elas são altamente eficazes em reduzir hospitalizações mesmo frente a essa nova inimiga: 96% de proteção para Pfizer e 92% para AstraZeneca.</p>
         <p> <b>Situação epidemiológica:</b> Se tornou a mais prevalente da Índia enquanto o país vivia uma devastadora segunda onda. Está sendo associada a um aumento de casos no Reino Unido, que já estava em plena reabertura de comércios e serviços. No Brasil, foram confirmados casos no Maranhão e no Paraná.</p>
-
+    </div>
 </div>
 </main>
 
