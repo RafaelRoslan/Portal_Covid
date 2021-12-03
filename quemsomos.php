bf710129034a8a204f67e2219df96a1cc303743f
Rafael Roslan <rafaelroslan@gmail.com>
2021-12-02 21:53:25 -0300

    layout quem somos

--- quemsomos.php
+++ quemsomos.php
@@ -1,22 +1,107 @@
 <?php include_once('includes/header.php'); ?>
 <!-- inicio do conteudo -->
 
-<main class="container text-justify">
-    <article class="col-10 col-sm-10 col-md-10 col-lg-10 offset-1">
+<main class="container text-justify altura">
+  <article class="col-10 col-sm-10 col-md-10 col-lg-10 offset-1">
 
-      <h2 class="text-center my-4">OS “ANBANDONADOS”</h2>
-      <p>Esse foi o título nos dados, com esse pequeno erro gramatical, seja  isso apenas um mero erro ao digitar ou algo proposital, essa palavra errada representa bem o que é um pequeno grupo esquecido que outrora era conhecido como “Grupo F”. Agora no segundo semestre do curso de Sistemas para Internet da Faculdade de Tecnologia de São Roque, a FATEC-SR, esse grupo deu início ao segundo projeto, agora com o tema: COVID-19.</p>
-      <p>Ainda no início do semestre, um fato em comum adicionou dois novo integrantes ao grupo, vindo de semestres diferentes por razões diferentes. Eles estavam igualmente abandonados. Com o grupo maior, irrompeu uma esperança acerca de um semestre mais plácido, porém algo inesperado sobreveio, o grupo possuía somente uma aula em comum, fazendo a comunicação e avanço desse projeto se dificultar. Somado a isso, a sombra de ser o último grupo, ainda abandonados e esquecidos por alguns professores, fez o grupo caminhar desordenadamente para a data final da entrega do projeto.</p>
-      <p>Contudo, nosso grupo persistiu, ainda que as vezes isolados e abandonados, nossa comunicação e organização se aprimoraram, o que fez bons laços e ótimas piadas surgirem. Cada integrante se mostrou valoroso, e mesmo em meio as dúvidas daqueles que nos guiam, nosso pequeno grupo menosprezado, ainda se mantem firme, tentando mostrar que podemos terminar o projeto</p>
+    <h2 class="text-center my-4">OS “ANBANDONADOS”</h2>
+    <p>Esse foi o título nos dados, com esse pequeno erro gramatical, seja isso apenas um mero erro ao digitar ou algo
+      proposital, essa palavra errada representa bem o que é um pequeno grupo esquecido que outrora era conhecido como
+      “Grupo F”. Agora no segundo semestre do curso de Sistemas para Internet da Faculdade de Tecnologia de São Roque, a
+      FATEC-SR, esse grupo deu início ao segundo projeto, agora com o tema: COVID-19.</p>
+    <p>Ainda no início do semestre, um fato em comum adicionou dois novo integrantes ao grupo, vindo de semestres
+      diferentes por razões diferentes. Eles estavam igualmente abandonados. Com o grupo maior, irrompeu uma esperança
+      acerca de um semestre mais plácido, porém algo inesperado sobreveio, o grupo possuía somente uma aula em comum,
+      fazendo a comunicação e avanço desse projeto se dificultar. Somado a isso, a sombra de ser o último grupo, ainda
+      abandonados e esquecidos por alguns professores, fez o grupo caminhar desordenadamente para a data final da
+      entrega do projeto.</p>
+    <p>Contudo, nosso grupo persistiu, ainda que as vezes isolados e abandonados, nossa comunicação e organização se
+      aprimoraram, o que fez bons laços e ótimas piadas surgirem. Cada integrante se mostrou valoroso, e mesmo em meio
+      as dúvidas daqueles que nos guiam, nosso pequeno grupo menosprezado, ainda se mantem firme, tentando mostrar que
+      podemos terminar o projeto</p>
 
-    </article>
+    <div class="text-end">
+      <h5><b>Do extinto grupo F,</b></h5>
+      <h5><b>os agora Mestres do Anbandono,</b></h5>
+      <h5><b>Gesonéis.</b></h5>
+    </div>
 
-    <aside class="col-3 offset-1">
-    
+    <aside class="row py-2 my-5 justify-content-evenly">
+
+      <section class="card mx-2 my-3 bg-dark bg-opacity-25 cardlargura">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="card my-3 mx-2 bg-dark bg-opacity-25 cardlargura">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="card my-3 mx-2 bg-dark bg-opacity-25 cardlargura">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="card my-3 mx-2 bg-dark bg-opacity-25 cardlargura">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </section>
 
 
     </aside>
+
+
+
+
+  </article>
+
+
 </main>
 
 <!-- fim do conteudo -->
-<?php include_once('includes/footer.php'); ?> 
+<?php include_once('includes/footer.php'); ?>
