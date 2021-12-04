bdb3354d3961bd4bf9e179d6caa132866adb576f
Rafael Roslan <rafaelroslan@gmail.com>
2021-12-04 01:00:31 -0300

    pequeno ajuste!

--- oquee.php
+++ oquee.php
@@ -1,35 +1,48 @@
 <?php include_once('includes/header.php'); ?>
 <!-- inicio do conteudo -->
-<main class="container text-justify">
+<main class="container text-justify my-4">
+  <div class="row justify-content-evenly">
+    <div class="col-10 col-sm-10 col-md-10 col-lg-3 p-3 ">
 
-  <article class="row justify-content-evenly ">
-    <div class="col-10 col-sm-10 col-md-10 col-lg-10 ">
-      <h2 class="text-center">O que é um vírus?</h2>
-      <p>Extremamente pequenos e simples, os vírus estão situados no limite entre vivo e não vivo. Eles diferem dos outros seres vivos porque não possuem estrutura celular nem metabolismo próprio. Quase todos os tipos de vírus medem menos de 200 nm de diâmetro, de modo que só podem ser observados com o auxílio de um microscópio.
-      Os vírus são constituídos principalmente por duas classes de substâncias químicas: proteínas e ácidos nucleicos e são considerados parasitas intracelulares obrigatórios, pois só se reproduzem no interior de um hospedeiro, onde ocorrem as infecções virais.</p>
-    </div>
-  </article>
-  <section class="row justify-content-evenly ">
-    <div class="col-10 col-sm-10 col-md-10 col-lg-10 ">
-      <h2 class="text-center">O que é a Covid-19?</h2>
-      <p>A Covid-19 é uma infecção respiratória viral aguda causada pelo coronavírus SARS-CoV-2, potencialmente grave, de
-        elevada transmissibilidade e de distribuição global.</p>
-      <p>O SARS-CoV-2 é um betacoronavírus descoberto em amostras de lavado broncoalveolar obtidas de pacientes com
-        pneumonia de causa desconhecida na cidade de Wuhan, província de Hubei, China, em dezembro de 2019. Pertence ao
-        subgênero Sarbecovírus da família Coronaviridae e é o sétimo coronavírus conhecido a infectar seres humanos.</p>
-      <p>Os coronavírus são uma grande família de vírus comuns em muitas espécies diferentes de animais, incluindo o
-         homem, camelos, gado, gatos e morcegos. Raramente os coronavírus de animais podem infectar pessoas e depois se
-         espalhar entre seres humanos como já ocorreu com o MERS-CoV e o SARS-CoV-2. Até o momento, não foi definido o
-         reservatório silvestre do SARS-CoV-2.</p>
-    </div>
-  </section>
-  <aside class="row justify-content-evenly">
-    <div class="col-12 col-sm-12 col-md-12 col-lg-8 ">
-      <img class="pul" src="imagens/pulmao.png">
-    </div>
-  </aside>
+      <aside class="row">
+          <h2 class="text-center">O que é um vírus?</h2>
+          <p>Extremamente pequenos e simples, os vírus estão situados no limite entre vivo e não vivo. Eles diferem dos
+            outros seres vivos porque não possuem estrutura celular nem metabolismo próprio. Quase todos os tipos de
+            vírus
+            medem menos de 200 nm de diâmetro, de modo que só podem ser observados com o auxílio de um microscópio.
+            Os vírus são constituídos principalmente por duas classes de substâncias químicas: proteínas e ácidos
+            nucleicos
+            e são considerados parasitas intracelulares obrigatórios, pois só se reproduzem no interior de um
+            hospedeiro,
+            onde ocorrem as infecções virais.</p>
+      </aside>
 
+      <aside class="row justify-content-center">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-8 ">
+          <img class="imglargura px-2" src="imagens/pulmao.png">
+        </div>
+      </aside>
 
+    </div>
+    <article class="col-10 col-sm-10 col-md-10 col-lg-6 bg-dark bg-opacity-25 rounded-3 px-5 py-4">
+     
+        <h2 class="text-center">O que é a Covid-19?</h2>
+        <p>A Covid-19 é uma infecção respiratória viral aguda causada pelo coronavírus SARS-CoV-2, potencialmente grave,
+          de
+          elevada transmissibilidade e de distribuição global.</p>
+        <p>O SARS-CoV-2 é um betacoronavírus descoberto em amostras de lavado broncoalveolar obtidas de pacientes com
+          pneumonia de causa desconhecida na cidade de Wuhan, província de Hubei, China, em dezembro de 2019. Pertence
+          ao
+          subgênero Sarbecovírus da família Coronaviridae e é o sétimo coronavírus conhecido a infectar seres humanos.
+        </p>
+        <p>Os coronavírus são uma grande família de vírus comuns em muitas espécies diferentes de animais, incluindo o
+          homem, camelos, gado, gatos e morcegos. Raramente os coronavírus de animais podem infectar pessoas e depois se
+          espalhar entre seres humanos como já ocorreu com o MERS-CoV e o SARS-CoV-2. Até o momento, não foi definido o
+          reservatório silvestre do SARS-CoV-2.</p>
+   
+    </article>
+
+  </div>
 </main>
 
 <!-- fim do conteudo -->
