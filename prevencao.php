2a5265109046ccacaf82338930dda9668f60026f
Anderson Silva <anderson_silva11000@outlook.com>
2021-12-05 20:43:06 -0300

    CSS e prevenção

--- prevencao.php
+++ prevencao.php
@@ -5,7 +5,7 @@
 
   <article class="row justify-content-center align-items-center">
     <h2 class="text-center"><b>Usar máscara de proteção, manter os cuidados básicos de higiene e evitar
-      aglomerações, são algumas forma de se proteger do Covid-19.</b></h2>
+        aglomerações, são algumas forma de se proteger do Covid-19.</b></h2>
 
     <h4 class="mt-5 offset-2"><b>COMO SE PROTEGER:</b></h4>
 
@@ -14,33 +14,31 @@
       <div class="col d-flex justify-content-center my-3">
         <div class="card mx-2 my-3 corcard cardlargura">
 
-          <img src="imagens/mascara" class="card-img-top caixaimg" alt="...">
-          <div class="card-body">
+          <img src="imagens/mascara" class="card-img-top caixaimg" alt="Mulher usando máscara descartavel">
+          <section class="card-body">
             <h5 class="card-title fontenegrito">USO DA MÁSCARA</h5>
 
-            <section>
               <p>O uso da máscara é fundamental para combater o Covid-19.</p>
-              <p> 1. A máscara é individual e não deve ser compartilhada. </p>
-              <p> 2. Use sempre que precisar sair de casa e lembre de levar uma reserva.</p>
-              <p> 3. Deve cobrir o nariz e a boca e ser usada de forma permanente no rosto, ou seja, não deve ser
+              <p>A máscara é individual e não deve ser compartilhada. </p>
+              <p>Use sempre que precisar sair de casa e lembre de levar uma reserva.</p>
+              <p>Deve cobrir o nariz e a boca e ser usada de forma permanente no rosto, ou seja, não deve ser
                 manipulada para o queixo ou pendurada em uma das orelhas, por exemplo.</p>
-              <p> 4. O ideal é que cada pessoa tenha pelo menos duas opções de máscaras de pano, em caso rompimento do
+              <p>O ideal é que cada pessoa tenha pelo menos duas opções de máscaras de pano, em caso rompimento do
                 elástico ou de umidade excessiva na mesma. </p>
-              <p> 5. Tenha cuidado com o descarte das máscaras descartáveis. Vede a máscara com uma sacola e jogue
+              <p>Tenha cuidado com o descarte das máscaras descartáveis. Vede a máscara com uma sacola e jogue
                 diretamente na lixeira. </p>
-            </section>
-          </div>
+
+          </section>
         </div>
 
       </div>
 
       <div class="col d-flex justify-content-center my-3">
         <div class="card mx-2 my-3 corcard cardlargura">
-          <img src="imagens/2" class="card-img-top caixaimg" alt="...">
-          <div class="card-body">
+          <img src="imagens/2" class="card-img-top caixaimg" alt="Lavando a mão com água e sabão">
+          <section class="card-body">
             <h5 class="card-title fontenegrito">HIGIENENIZAÇÃO</h5>
 
-            <section>
               <p>Medidas básicas na rotina diária, como lavar as mãos são fundamentais para a redução do contágio.</p>
               <p> O vírus também pode ser transferido de uma superfície para outra por meio das mãos contaminadas, o que
                 facilita a transmissão por contato indireto.</p>
@@ -50,19 +48,16 @@
               <p>No caso específico da higienização, é recomendado o uso do álcool a 70% como desinfetante e
                 antissépticos para as mãos. </p>
             </section>
-          </div>
         </div>
       </div>
 
 
       <div class="col d-flex justify-content-center my-3">
         <div class="card mx-2 my-3 corcard cardlargura">
-          <img src="imagens/distanciamento" class="card-img-top caixaimg" alt="...">
-          <div class="card-body">
+          <img src="imagens/distanciamento" class="card-img-top caixaimg" alt="Pessoas na fila com distanciamento entre elas">
+          <section class="card-body">
             <h5 class="card-title fontenegrito">DISTANCIAMENTO SOCIAL</h5>
 
-
-            <section>
               <p>Limitar o contato próximo entre pessoas infectadas e outras pessoas é importante para reduzir as
                 chances de transmissão. Seguir as medidas de distanciamento diminui a velocidade de transmissão do
                 vírus. </p>
@@ -74,18 +69,16 @@
               <p>Garantir uma boa ventilação em ambientes internos também é uma medida importante para prevenir a
                 transmissão em ambientes coletivos.</p>
             </section>
-          </div>
 
         </div>
       </div>
 
       <div class="col d-flex justify-content-center my-3">
         <div class="card mx-2 my-3 corcard cardlargura">
-          <img src="imagens/3" class="card-img-top caixaimg" alt="...">
-          <div class="card-body">
+          <img src="imagens/3" class="card-img-top caixaimg" alt="Pessoas se higienizando no ambiente de trabalho">
+          <section class="card-body">
             <h5 class="card-title fontenegrito">CUIDADOS NO AMBIENTE DE TRABALHO</h5>
 
-            <section>
               <p>No ambiente de trabalho é importante estar atento à higiene. O recomendado é que as empresas e
                 organizações disponibilizem recursos para a higienização das mãos nos locais de trabalho, incluindo
                 água, sabonete líquido, toalha de papel descartável e lixeira, com abertura que não demande contato
@@ -97,41 +90,38 @@
                 o uso do refeitório e seja mantido o espaço mínimo de um metro entre as pessoas nas filas e mesas. No
                 caso dos banheiros, deve-se evitar a aglomeração.</p>
             </section>
-          </div>
         </div>
       </div>
 
       <div class="col d-flex justify-content-center my-3">
         <div class="card mx-2 my-3 corcard cardlargura">
-          <img src="imagens/tosse" class="card-img-top caixaimg" alt="...">
-          <div class="card-body">
+          <img src="imagens/tosse" class="card-img-top caixaimg" alt="Mulher cobrindo a boca com o braço ao tossir">
+          <section class="card-body">
             <h5 class="card-title fontenegrito">CUBRA TOSSES E ESPIRROS</h5>
 
-            <section>
+            
               <p>Se você não estiver de máscaras, lembre-se de sempre cobrir a boca e o nariz com um lenço de papel
                 quando tossir ou espirrar ou usar a parte interna do cotovelo e não cuspir.</p>
               <p>Jogue os lenços de papel e outros tecidos usados ​​no lixo.</p>
               <p>Lembre-se de lavar imediatamente as mãos.</p>
-            </section>
-          </div>
+
+          </section>
         </div>
       </div>
 
 
       <div class="col d-flex justify-content-center my-3">
         <div class="card mx-2 my-3 corcard cardlargura">
-          <img src="imagens/4" class="card-img-top caixaimg" alt="...">
-          <div class="card-body">
+          <img src="imagens/4" class="card-img-top caixaimg" alt="Rosto com protetor solar">
+          <section class="card-body">
             <h5 class="card-title fontenegrito">CUIDADOS COMPLEMENTARES</h5>
 
-
-            <section>
               <p>O álcool 70% é um importante aliado de higienização na luta contra a Covid-19. Mas é importante estar
                 atento ao seu uso para evitar queimaduras. </p>
               <p>Ao usar a máscara, passe protetor solar para evitar marcas de queimaduras de sol indesejáveis no rosto.
               </p>
             </section>
-          </div>
+
         </div>
       </div>
     </div>
