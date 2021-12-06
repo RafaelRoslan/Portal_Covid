0d85af3fba1021d33f48ac14e212678c54d07fe3
Felipe <goesfelipesantos@gmail.com>
2021-12-06 18:15:00 -0300

    alterações finais css

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/gejoneis.css">
+  <link rel="stylesheet" href="css/style.css">
 
   <title>Portal Covid</title>
 </head>
